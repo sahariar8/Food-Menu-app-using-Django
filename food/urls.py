ed12232fbc7d29642df4bedec404130041de7626
shahariar <shahariar@inneed.cloud>
2024-05-25 18:52:54 +0600

    dynamic detail page create

--- food/urls.py
+++ food/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from . import views
 
+app_name = 'food'
 
 urlpatterns = [
-    path('',views.home,name='home')
+    path('',views.home,name='home'),
+    path('item',views.item,name='item'),
+    path('item/<int:item_id>',views.detail,name='detail')
 ]
--- food/views.py
+++ food/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Item
 
 # Create your views here.
 def home(request):
-    return HttpResponse("hello shahariar")
+    items = Item.objects.all()
+    return render(request,'food/index.html',{'items':items})
+
+def item(request):
+    return HttpResponse("<h1>This is an Item View</h1>")
+
+def detail(request,item_id):
+    item = Item.objects.get(pk = item_id)
+    return render(request,'food/detail.html',{'item':item})
